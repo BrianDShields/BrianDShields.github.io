a7728bad97f3f3b76db564e4f22d41e89064f353
Brian Shields <BrianShields25@gmail.com>
2015-07-20 18:21:33 -0500

    Initial Commit
    MVPs for MBAs

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+
+
+
+$(function(){
+
+		$('.SignUp').on('click', function() {
+		$(this).find('section').toggle();
+		$(this).find('.Email').toggle();
+	});
+
+	console.log("Loaded")
+
+	$('.BuildSomething').on('click', function() {
+		$(this).find('section').toggle();
+		$(this).find('.Email').toggle();
+	});
+
+	console.log("Loaded")
+
+
+
+
+
+
+
+
+
+});
