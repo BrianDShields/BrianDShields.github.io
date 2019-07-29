2a6abac0a2e5526fd74221d8eff34982113ae4cc
Brian Shields <BrianShields25@gmail.com>
2019-07-29 11:18:55 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -2,11 +2,11 @@
 $(document).ready(function(){
     $(".button a").click(function(){
         $(".overlay").fadeToggle(200);
-       $(this).toggleClass('btn-open').toggleClass('btn-close');
+       $(this).toggleClass('topnav-right-home').toggleClass('topnav-right-home-hidden');
     });
 });
 $('.overlay').on('click', function(){
     $(".overlay").fadeToggle(200);   
-    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+    $(".button a").toggleClass('topnav-right-home').toggleClass('topnav-right-home-hidden');
     open = false;
 });
